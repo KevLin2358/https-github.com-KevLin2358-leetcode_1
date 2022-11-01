0964bf5bd7a05dcb131d6ee7bbf630ff49c2aa58
KevLin2358 <47483994+KevLin2358@users.noreply.github.com>
2022-11-01 13:58:21 -0400

    Time: 111 ms (77.01%), Space: 44.6 MB (68.66%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+    if(nums.length === 0) return 0;
+    let j = 1;
+    
+    for(let i = 0; i < nums.length - 1; i++){
+        if(nums[i] !== nums[i+1]){
+            nums[j] = nums[i+1]
+            j++;
+        }
+    }
+    
+    return j;
+};
